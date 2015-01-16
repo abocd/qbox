e1c0ce16d227cae06136282fa3861fc6038b4c09
aboc <abocd@qq.com>
2015-01-16 16:29:37 +0800

    新增皮肤功能 bug修复 演示文档增加样式

--- README.md
+++ README.md
@@ -1,39 +1,39 @@
-# qbox
-#####作者:aboc Email:mayinhua@gmail.com
-[我的php之旅](http://www.phpec.org)
-
-##轻弹窗的使用说明
-
-1. 引入JS文件
-   ```javascript
-   <script type="text/javascript" src="js/qbox.js"></script>
-```
-2. 初始化qbox
-   ```javascript
-   var box = new qbox();
-   ```
-3. 使用
-```javascript
-//确认的
-box.alert("我出来了,点击确定弹出alert",function(d){
-            if(d){
-                alert("弹出了一个alert");
-            }
-        },{
-            title:'自定义的提示标题'
-        });
-//确认和取消的
-box.confirm("确定和取消弹出的提示不一样哦",function(d){
-            if(d){
-                alert("yes");
-            } else {
-                alert("no")
-            }
-        },{
-            title:'自定义的提示标题'
-        });
-```
-
-是不是很简单呢?
-
+# qbox
+#####作者:aboc Email:mayinhua@gmail.com
+[我的php之旅](http://www.phpec.org)
+
+##轻弹窗的使用说明
+
+1. 引入JS文件
+   ```javascript
+   <script type="text/javascript" src="js/qbox.js"></script>
+```
+2. 初始化qbox
+   ```javascript
+   var box = new qbox();
+   ```
+3. 使用
+```javascript
+//确认的
+box.alert("我出来了,点击确定弹出alert",function(d){
+            if(d){
+                alert("弹出了一个alert");
+            }
+        },{
+            title:'自定义的提示标题'
+        });
+//确认和取消的
+box.confirm("确定和取消弹出的提示不一样哦",function(d){
+            if(d){
+                alert("yes");
+            } else {
+                alert("no")
+            }
+        },{
+            title:'自定义的提示标题'
+        });
+```
+
+是不是很简单呢?
+
 演示地址:[https://abocd.github.io/qbox/demo.html](https://abocd.github.io/qbox/demo.html)
--- js/qbox.js
+++ js/qbox.js
@@ -5,23 +5,37 @@ function qbox(){
     this.param = {
         title :"系统消息",
         value:"确定",
-        cancel:"取消"
+        cancel:"取消",
+        themes:"default_box"
     };
-    this.callback = null;
+    //皮肤
+    this.themes = {
+        default_box:'#szxbox{color:#fff;font-size: 14px;width:98%;}#szxbox h1.szxheader{font-size: 15px;padding:0;line-height: 2;border-bottom: 1px solid #aaa;width: 98%;margin:0 auto;}#szxbox .szxcontent{line-height: 1.8;font-size: 13px;margin-top:10px;}#szxbox .szxbottom{margin:10px 0px 8px;}#szxbox .szxbutton{font-size:12px;display: inline-block;padding: 2px 5px;background: #999;color: #333;cursor: pointer;margin-left:3px;margin-right:3px;}#szxbox .szxbutton.szxsubmit{background: #afafaf}',
+        //小型
+        default_min:'#szxbox{color:#fff;font-size: 14px;min-width: 350px;}#szxbox h1.szxheader{font-size: 15px;padding:0;line-height: 2;border-bottom: 1px solid #aaa;margin:0 auto;}#szxbox .szxcontent{line-height: 1.8;font-size: 13px;margin-top:10px;}#szxbox .szxbottom{margin:10px 0px 8px;}#szxbox .szxbutton{font-size:12px;display: inline-block;padding: 2px 5px;background: #999;color: #333;cursor: pointer;margin-left:3px;margin-right:3px;}#szxbox .szxbutton.szxsubmit{background: #afafaf}'
+    };
+    //确定按钮
     this.submit = function(click){
         if(typeof click=="undefined"){
-            click = "box.close(true)";
+            click = "qbox.close(true)";
         }
         if(click == ""){
             return '';
         }
+        if(this.param.value == ""){
+            return '';
+        }
         return '<span class="szxbutton szxsubmit" onclick="'+click+'">'+this.param.value+'</span>';
     }
+    //取消按钮
     this.cancel = function(click){
         if(typeof click=="undefined"){
-            click = "box.close(false)";
+            click = "qbox.close(false)";
         }
         if(click == ""){
+            return '';
+        }
+        if(this.param.cancel == ""){
             return '';
         }
         return '<span class="szxbutton" onclick="'+click+'">'+this.param.cancel+'</span>';
@@ -30,21 +44,18 @@ function qbox(){
         if(typeof param == "undefined"){
             param = {};
         }
-        if(typeof callback == "function"){
-            this.callback = callback;
-        }
+        qbox.callback = callback;
+
         this.param = this.extend(this.param,param,true);
-        this.box(str,this.submit(),callback);
+        this.box(str,this.submit());
     }
     this.confirm = function(str,callback,param){
         if(typeof param == "undefined"){
             param = {};
         }
-        if(typeof callback == "function"){
-            this.callback = callback;
-        }
+        qbox.callback = callback;
         this.param = this.extend(this.param,param,true);
-        this.box(str,this.submit()+this.cancel(),callback);
+        this.box(str,this.submit()+this.cancel());
     }
     this.box = function(str,bottom){
         var szxbox = document.getElementById("szxbox");
@@ -52,17 +63,16 @@ function qbox(){
             this.close();
         }
         if(typeof bottom== "undefined" || bottom==""){
-            bottom = '<span class="szxbutton" onclick="box.close()">确定</span>';
+            bottom = this.submit();
         }
-        html = '<style>#szxbox{color:#fff;font-size: 14px;}#szxbox h1.szxheader{font-size: 15px;padding:0;line-height: 2;border-bottom: 1px solid #aaa;width: 98%;margin:0 auto;}#szxbox .szxcontent{line-height: 1.8;font-size: 13px;margin-top:10px;}#szxbox .szxbottom{margin:10px 0px 8px;}#szxbox .szxbutton{font-size:12px;display: inline-block;padding: 2px 5px;background: #999;color: #333;cursor: pointer;margin-left:3px;margin-right:3px;}#szxbox .szxbutton.szxsubmit{background: #afafaf}</style>';
+        html = '<style>'+this.themes[this.param.themes]+'</style>';
         html += '<div style="text-align: center;line-height: 1.5;min-height: 60px;"><h1 class="szxheader">'+this.param.title+'</h1><div class="szxcontent">'+str+'</div><div class="szxbottom">'+bottom+'</div></div>';
         var client_width = document.body.clientWidth;
         var szxbox = document.createElement("div");
         szxbox.setAttribute("id","szxbox");
-        szxbox.style.position = "absolute";
+        szxbox.style.position = "fixed";
         szxbox.style.margin = "0 auto;";
         szxbox.style.top = "32%";
-        szxbox.style.width= (client_width-20)+"px";
         szxbox.style.background="#666";
         szxbox.style.filter="progid:DXImageTransform.Microsoft.Alpha(style=3,opacity=25,finishOpacity=90";
         szxbox.style.opacity="0.90";
@@ -73,27 +83,11 @@ function qbox(){
         document.body.appendChild(szxbox);
         szxbox = document.getElementById("szxbox");
 //                alert(szxbox.style.width);
-        var boxwidth = parseInt(szxbox.style.width);
-
+        var boxwidth = parseInt(szxbox.offsetWidth);
+        szxbox.style.left = ((client_width-boxwidth)/2)+"px";
     }
 
-    /**
-     * 关闭
-     *
-     **/
-    this.close = function(submit){
-        var szxbox = document.getElementById("szxbox");
-        if(szxbox!=null){
-            szxbox.remove();
-        }
-        if(typeof submit=="undefined"){
-            submit = false;
-        }
-        if(this.callback != null){
-            this.callback.call(this,submit)
-        }
-        return true;
-    }
+
     /**
      * 合并对象
      * @param des
@@ -113,4 +107,20 @@ function qbox(){
         }
         return des;
     }
+}
+//回调函数
+qbox.callback = null;
+//关闭
+qbox.close = function(submit){
+    var szxbox = document.getElementById("szxbox");
+    if(szxbox!=null){
+        szxbox.remove();
+    }
+    if(typeof submit=="undefined"){
+        submit = false;
+    }
+    if(qbox.callback != null){
+        qbox.callback.call(qbox,submit)
+    }
+    return true;
 }
