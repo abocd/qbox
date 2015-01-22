474f0d2cd4cab4548e239c75bce86fc1a0c594c4
aboc <abocd@qq.com>
2015-01-22 17:42:25 +0800

    增加背景遮罩功能

--- js/qbox.js
+++ js/qbox.js
@@ -4,16 +4,22 @@
  */
 function qbox(config){
     this.param = {
+        //标题
         title :"系统消息",
+        //确定按钮
         value:"确定",
+        //取消按钮
         cancel:"取消",
-        themes:"default_box"
+        //皮肤
+        themes:"default_box",
+        //遮罩,
+        mask:false
     };
     //皮肤
     this.themes = {
-        default_box:'#szxbox{color:#fff;font-size: 14px;width:98%;}#szxbox h1.szxheader{font-size: 15px;padding:0;line-height: 2;border-bottom: 1px solid #aaa;width: 98%;margin:0 auto;}#szxbox .szxcontent{line-height: 1.8;font-size: 13px;margin-top:10px;}#szxbox .szxbottom{margin:10px 0px 8px;}#szxbox .szxbutton{font-size:12px;display: inline-block;padding: 2px 5px;background: #999;color: #333;cursor: pointer;margin-left:3px;margin-right:3px;}#szxbox .szxbutton.szxsubmit{background: #afafaf}',
+        default_box:'#qbmask{position:absolute;background:#ddd;filter:alpha(opacity=50); -moz-opacity:0.5;-khtml-opacity: 0.5; opacity: 0.5;height:100%;width:100%;z-index:999998;top:0;}#qbbox{color:#fff;font-size: 14px;width:98%;}#qbbox h1.qbheader{font-size: 15px;padding:0;line-height: 2;border-bottom: 1px solid #aaa;width: 98%;margin:0 auto;}#qbbox .qbcontent{line-height: 1.8;font-size: 13px;margin-top:10px;}#qbbox .qbbottom{margin:10px 0px 8px;}#qbbox .qbbutton{font-size:12px;display: inline-block;padding: 2px 5px;background: #999;color: #333;cursor: pointer;margin-left:3px;margin-right:3px;}#qbbox .qbbutton.qbsubmit{background: #afafaf}',
         //小型
-        default_min:'#szxbox{color:#fff;font-size: 14px;min-width: 350px;}#szxbox h1.szxheader{font-size: 15px;padding:0;line-height: 2;border-bottom: 1px solid #aaa;margin:0 auto;}#szxbox .szxcontent{line-height: 1.8;font-size: 13px;margin-top:10px;}#szxbox .szxbottom{margin:10px 0px 8px;}#szxbox .szxbutton{font-size:12px;display: inline-block;padding: 2px 5px;background: #999;color: #333;cursor: pointer;margin-left:3px;margin-right:3px;}#szxbox .szxbutton.szxsubmit{background: #afafaf}'
+        default_min:'#qbbox{color:#fff;font-size: 14px;min-width: 350px;}#qbbox h1.qbheader{font-size: 15px;padding:0;line-height: 2;border-bottom: 1px solid #aaa;margin:0 auto;}#qbbox .qbcontent{line-height: 1.8;font-size: 13px;margin-top:10px;}#qbbox .qbbottom{margin:10px 0px 8px;}#qbbox .qbbutton{font-size:12px;display: inline-block;padding: 2px 5px;background: #999;color: #333;cursor: pointer;margin-left:3px;margin-right:3px;}#qbbox .qbbutton.qbsubmit{background: #afafaf}'
     };
     //确定按钮
     this.submit = function(click){
@@ -26,7 +32,7 @@ function qbox(config){
         if(this.param.value == ""){
             return '';
         }
-        return '<span class="szxbutton szxsubmit" onclick="'+click+'">'+this.param.value+'</span>';
+        return '<span class="qbbutton qbsubmit" onclick="'+click+'">'+this.param.value+'</span>';
     }
     //取消按钮
     this.cancel = function(click){
@@ -39,7 +45,7 @@ function qbox(config){
         if(this.param.cancel == ""){
             return '';
         }
-        return '<span class="szxbutton" onclick="'+click+'">'+this.param.cancel+'</span>';
+        return '<span class="qbbutton" onclick="'+click+'">'+this.param.cancel+'</span>';
     }
     this.alert = function(str,callback,param){
         if(typeof param == "undefined"){
@@ -59,33 +65,42 @@ function qbox(config){
         this.box(str,this.submit()+this.cancel());
     }
     this.box = function(str,bottom){
-        var szxbox = document.getElementById("szxbox");
-        if(szxbox!=null){
+        var qbbox = document.getElementById("qbbox");
+        if(qbbox!=null){
             this.close();
         }
         if(typeof bottom== "undefined" || bottom==""){
             bottom = this.submit();
         }
-        html = '<style>'+this.themes[this.param.themes]+'</style>';
-        html += '<div style="text-align: center;line-height: 1.5;min-height: 60px;"><h1 class="szxheader">'+this.param.title+'</h1><div class="szxcontent">'+str+'</div><div class="szxbottom">'+bottom+'</div></div>';
+        var html = '<style>'+this.themes[this.param.themes]+'</style>';
+        html += '<div style="text-align: center;line-height: 1.5;min-height: 60px;"><h1 class="qbheader">'+this.param.title+'</h1><div class="qbcontent">'+str+'</div><div class="qbbottom">'+bottom+'</div></div>';
         var client_width = document.body.clientWidth;
-        var szxbox = document.createElement("div");
-        szxbox.setAttribute("id","szxbox");
-        szxbox.style.position = "fixed";
-        szxbox.style.margin = "0 auto;";
-        szxbox.style.top = "32%";
-        szxbox.style.background="#666";
-        szxbox.style.filter="progid:DXImageTransform.Microsoft.Alpha(style=3,opacity=25,finishOpacity=90";
-        szxbox.style.opacity="0.90";
-        szxbox.style.left = "10px";
-        szxbox.style.zIndex = "999999";
-        szxbox.innerHTML = html;
-        //szxbox.style.display = "none";
-        document.body.appendChild(szxbox);
-        szxbox = document.getElementById("szxbox");
-//                alert(szxbox.style.width);
-        var boxwidth = parseInt(szxbox.offsetWidth);
-        szxbox.style.left = ((client_width-boxwidth)/2)+"px";
+        var qbbox = document.createElement("div");
+        qbbox.setAttribute("id","qbbox");
+        qbbox.style.position = "fixed";
+        qbbox.style.margin = "0 auto;";
+        qbbox.style.top = "32%";
+        qbbox.style.background="#666";
+        qbbox.style.filter="progid:DXImageTransform.Microsoft.Alpha(style=3,opacity=25,finishOpacity=90";
+        qbbox.style.opacity="0.90";
+        qbbox.style.left = "10px";
+        qbbox.style.zIndex = "999999";
+        qbbox.innerHTML = html;
+        //qbbox.style.display = "none";
+        document.body.appendChild(qbbox);
+        if(this.param.mask){
+            //有遮罩
+            html += '<div class="qbmask"></div>';
+            var qbmask = document.createElement("div");
+            qbmask.setAttribute("id","qbmask");
+            document.body.appendChild(qbmask);
+            qbmask = document.getElementById("qbmask");
+            qbmask.style.height = document.body.offsetHeight+"px";
+        }
+        qbbox = document.getElementById("qbbox");
+//                alert(qbbox.style.width);
+        var boxwidth = parseInt(qbbox.offsetWidth);
+        qbbox.style.left = ((client_width-boxwidth)/2)+"px";
     }
 
 
@@ -131,8 +146,12 @@ qbox.close = function(submit){
     return true;
 }
 qbox.hide = function(){
-    var szxbox = document.getElementById("szxbox");
-    if(szxbox!=null){
-        szxbox.remove();
+    var qbbox = document.getElementById("qbbox");
+    if(qbbox!=null){
+        qbbox.remove();
+    }
+    var qbmask = document.getElementById("qbmask");
+    if (qbmask != null) {
+        qbmask.remove();
     }
 }
